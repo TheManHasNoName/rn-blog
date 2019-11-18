fbced3e2bfee0dc1b8a45f3b7f3654f62a89cef0
Xavier Johanis <xavierjohanis@gmail.com>
2019-11-18 21:28:56 +0800

    add styling and icon

--- src/screens/IndexScreens.js
+++ src/screens/IndexScreens.js
@@ -1,24 +1,45 @@
 import React, {useContext} from 'react';
 import {Text, View, StyleSheet, FlatList, Button} from 'react-native';
 import {Context} from '../context/BlogContext';
+import { AntDesign } from '@expo/vector-icons'
 
 const IndexScreen = () => {
     const { state, addBlogPost} = useContext(Context);
 
     return (
         <View>
-            <Text>Index Screen</Text>
             <Button title='Add Post' onPress={addBlogPost}/>
             <FlatList
                 data={state}
                 keyExtractor={(blogPost) => blogPost.title}
                 renderItem={({item}) => {
-                    return <Text>{item.title}</Text>
+                    return (
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <AntDesign style={styles.icon}  name='delete' />
+                        </View>
+                    )
                 }}
             />
         </View>
     )
 }
 
-const styles=StyleSheet.create({});
+const styles=StyleSheet.create({
+    row: {
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        borderTopWidth: 1,
+        borderColor: 'gray',
+        paddingHorizontal: 10,
+        paddingVertical: 20
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 35
+    }
+});
 export default IndexScreen;
