87d8ae796554da3c93f1735d235408f37e6983f0
Xavier Johanis <xavierjohanis@gmail.com>
2019-11-19 18:57:35 +0800

    delete blogpost

--- src/screens/IndexScreens.js
+++ src/screens/IndexScreens.js
@@ -1,10 +1,10 @@
 import React, {useContext} from 'react';
-import {Text, View, StyleSheet, FlatList, Button} from 'react-native';
+import {Text, View, StyleSheet, FlatList, Button, TouchableOpacity} from 'react-native';
 import {Context} from '../context/BlogContext';
 import { AntDesign } from '@expo/vector-icons'
 
 const IndexScreen = () => {
-    const { state, addBlogPost} = useContext(Context);
+    const { state, addBlogPost, deleteBlogPost} = useContext(Context);
 
     return (
         <View>
@@ -15,8 +15,10 @@ const IndexScreen = () => {
                 renderItem={({item}) => {
                     return (
                         <View style={styles.row}>
-                            <Text style={styles.title}>{item.title}</Text>
-                            <AntDesign style={styles.icon}  name='delete' />
+                            <Text style={styles.title}>{item.title} - {item.id}</Text>
+                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                <AntDesign style={styles.icon}  name='delete' />
+                            </TouchableOpacity>
                         </View>
                     )
                 }}
