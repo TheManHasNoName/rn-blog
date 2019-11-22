bec83d33b1f44d10de3c8ed87ad7368dff0c020e
Xavier Johanis <xavierjohanis@gmail.com>
2019-11-22 22:08:01 +0800

    add post using navigation

--- src/screens/IndexScreens.js
+++ src/screens/IndexScreens.js
@@ -29,6 +29,16 @@ const IndexScreen = ({navigation}) => {
     )
 }
 
+IndexScreen.navigationOptions = ({ navigation }) => {
+    return {
+        headerRight: (
+            <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+                <AntDesign size={30} name='plus'/>
+            </TouchableOpacity>
+        )
+    }
+}
+
 const styles=StyleSheet.create({
     row: {
         flexDirection: "row",
